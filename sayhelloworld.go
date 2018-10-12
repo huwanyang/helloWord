2998c92e55d25268205d894e65709b25244ef790
wanyang3 <wanyang3@staff.weibo.com>
2018-10-12 11:06:38 +0800

    add sayhelloworld.go file

---
+++ sayhelloworld.go
@@ -0,0 +1,15 @@
+package helloword
+
+import "fmt"
+
+func SayHelloWorld() {
+	fmt.Printf("%s", "Hello world!")
+}
+
+func Version() string {
+	return "v1.0.0"
+}
+
+func SayOther(str string) {
+	fmt.Printf("Saying %s", str)
+}
